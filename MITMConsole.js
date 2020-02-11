e85ed1635fa8a60ac027817dfccdf20c7f783d13
^_^ <44122582+ReFormationPro@users.noreply.github.com>
2020-02-11 18:59:55 +0300

    Added fixed sized packet storage

--- MITMConsole.js
+++ MITMConsole.js
@@ -1,20 +1,50 @@
-var MITM = {
-  // Logs its arguments. Arguments are in the same order with flash.net.Socket method arguments
-  // and they are followed by the method name.
-	mainListener: function() {
-		console.log(arguments);
-		return arguments[0];
-	}
-	
+const MITM = {
+    initialize: function() {
+        this.createMITMListeners();
+        this.messageArr = new FixedLengthArray(this.MAX_MESSAGE_COUNT);
+    },
+    createMITMListeners: function() {
+        // Create listeners from HOOKED_METHODS
+        this.HOOKED_METHODS.forEach(l => {
+            this[l] = listener.bind(null, l);
+        });
+        function listener(methodName) {
+    		var packet = Array.prototype.map.call(arguments, (a) => {return a});
+		    return MITM.mainListener(methodName, packet);
+        }
+    },
+    // All listeners are redirected to here for now.
+    // Packet is the arguments of the hooked method in the same order with flash.net.Socket method arguments.
+    // You can change return value (original is packet[0] to alter behaviour.
+    mainListener: function(methodName, packet) {
+        this.messageArr.push(packet);
+        console.log.apply(null, packet);
+        return packet[0];
+        //TODO: GUI
+    },
+    MAX_MESSAGE_COUNT: 100,
+    // Hooked Methods Array
+    HOOKED_METHODS: ["readBoolean","readByte","readBytes","readDouble","readFloat","readInt","readMultiByte","readObject","readShort","readUnsignedByte","readUnsignedInt","readUnsignedShort","readUTF","readUTFBytes","writeBoolean","writeByte","writeBytes","writeDouble","writeFloat","writeInt","writeMultiByte","writeObject","writeShort","writeUnsignedInt","writeUTF","writeUTFBytes"]
+};
+/**
+  * If one adds elements in a way like arr[arr.length], we can do nothing about it.
+  */
+class FixedLengthArray extends Array {
+    constructor(size) {
+        super();
+        this.size = size;
+    }
+    push(element) {
+        while (this.size >= this.length) {
+            super.shift();
+        }
+        super.push(element);
+    }
+    unshift(element) {
+        while (this.size >= this.length) {
+            super.pop();
+        }
+        super.unshift(element);
+    }
 }
-// Hooked Methods Array
-var methods = ["readBoolean","readByte","readBytes","readDouble","readFloat","readInt","readMultiByte","readObject","readShort","readUnsignedByte","readUnsignedInt","readUnsignedShort","readUTF","readUTFBytes","writeBoolean","writeByte","writeBytes","writeDouble","writeFloat","writeInt","writeMultiByte","writeObject","writeShort","writeUnsignedInt","writeUTF","writeUTFBytes"];
-methods.forEach(m => {
-  // Creates method listeners. If you want to disable some of them, remove them from the array above.
-  // Or you may type "delete MITM.methodName" into your console later.
-	MITM[m] = function () {
-		var arr = Array.prototype.map.call(arguments, (a) => {return a});
-		arr.push(m);
-		return MITM.mainListener.apply(null, arr);
-	};
-});
+MITM.initialize();
